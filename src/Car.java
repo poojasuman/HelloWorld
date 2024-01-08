e685a631d5ce68a7463099691f84308164dd82c8
poojasuman <poojasuman212@gmail.com>
2024-01-07 21:36:29 -0800

    class Getter Setter.

---
+++ src/Car.java
@@ -0,0 +1,47 @@
+public class Car {
+    private String make;
+    private String model;
+    private String color;
+    private int doors;
+    private boolean isConvertible;
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return isConvertible;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.isConvertible = convertible;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,34 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        boolean isAlien = false;
+        //if else example
+        if (isAlien == false) {
+            System.out.println("It is not Alien");
+        } else {
+            System.out.println("It is Alien");
+        }
+        //Ternary operator
+        boolean isDomestic = isAlien ? false : true;
+        System.out.println("Person is Domestic " + isDomestic);
+        //Method call
+        int score = calculateScore(true, 800, 5, 100);
+
+        Car car = new Car();
+        car.setMake("Porshe");
+        car.setModel("Maccan");
+        System.out.println("car model is " + car.getModel());
+        System.out.println("Car make is " + car.getMake());
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        int finalScore = score;
+        if(gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score is " + finalScore);
+        }
+        return finalScore;
     }
 }
